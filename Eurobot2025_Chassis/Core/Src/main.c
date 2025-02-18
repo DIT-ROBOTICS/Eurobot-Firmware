b6fd9fc0ee3185c684b59daab3a9248c2c2a509d
Rabbit025879 <rabbit025879@gmail.com>
2025-02-18 14:36:12 +0800

    Fix: Failed UART communication error

--- Eurobot2025_Chassis/Core/Src/main.c
+++ Eurobot2025_Chassis/Core/Src/main.c
@@ -120,11 +120,11 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
-  MX_USART1_UART_Init();
   MX_TIM12_Init();
   MX_TIM15_Init();
   MX_TIM13_Init();
   MX_DMA_Init();
+  MX_USART1_UART_Init();
   MX_TIM7_Init();
   MX_TIM1_Init();
   MX_TIM5_Init();
